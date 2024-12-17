edeb4581665a17df60098392afe2095b808b5d34
Kaneku Ren <e235749@ie.u-ryukyu.ac.jp>
2024-12-17 21:04:36 +0900

    second commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        String str = null;
+
+        try{
+            System.out.println(str.length());
+
+        }catch(NullPointerException e){
+            System.out.println("エラーが発生しました。" + e.getMessage());
+        }
+    }
+}
